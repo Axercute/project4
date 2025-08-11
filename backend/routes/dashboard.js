402edfb2fd92c2fcd5458878711b4d20bf5e2de0
John <axercute@gmail.com>
2025-08-11 14:17:03 +0800

    Great let's test more

--- backend/routes/dashboard.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardCtrl = require('../controllers/dashboard');
-const securityMiddleware = require('../middlewares/securityMiddleware');
-router.get('/', securityMiddleware.checkLogin, dashboardCtrl.getDashboardData);
-module.exports = router;
